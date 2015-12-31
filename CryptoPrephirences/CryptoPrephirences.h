769b38fa9ca646f6033bcd302a2ca3c657bcc2e3
phimage <ssjgotrunk@yahoo.fr>
2015-12-31 11:10:13 +0100

    Initial Commit

---
+++ CryptoPrephirences/CryptoPrephirences.h
@@ -0,0 +1,19 @@
+//
+//  CryptoPrephirences.h
+//  CryptoPrephirences
+//
+//  Created by phimage on 31/12/15.
+//  Copyright © 2015 phimage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CryptoPrephirences.
+FOUNDATION_EXPORT double CryptoPrephirencesVersionNumber;
+
+//! Project version string for CryptoPrephirences.
+FOUNDATION_EXPORT const unsigned char CryptoPrephirencesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CryptoPrephirences/PublicHeader.h>
+
+
